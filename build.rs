108de07044af9d62e28ff24e21edaad8e5305875
Roee Shapira <roy.shap1@gmail.com>
2021-08-21 13:21:59 +0300

    Improved glue file

--- build.rs
+++ build.rs
@@ -1,16 +1,17 @@
-use std::{env, fs, io, path::Path};
+use std::{env, fs, io::{self, Write}, path::Path};
 
 fn main() -> io::Result<()> {
 	let out_dir = env::var_os("OUT_DIR").unwrap();
 	let dest_path = Path::new(&out_dir).join("pj.sh");
-	fs::write(
-		&dest_path,
-		r#"
+
+    let mut file = fs::File::create(&dest_path)?;
+	file.write_all(
+		b"
         #!/bin/sh
 
         ./pj
-        alias ${PJ_CMD:-pj}='./pj --add "$1" && cd "$_"'
-        "#,
+        alias ${PJ_CMD:-pj}='./pj --add \"$1\" && cd \"$_\"'
+        ",
 	)?;
 
 	println!("cargo:rerun-if-changed=build.rs");
