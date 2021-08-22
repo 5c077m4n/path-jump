11b80aa27b547aa5cd8ea0e9117d85306ea5998b
Roee Shapira <roy.shap1@gmail.com>
2021-08-22 23:34:28 +0300

    Added .editorconfig

--- resources/install.sh
+++ resources/install.sh
@@ -4,9 +4,9 @@ __J="$(pwd)/$(dirname "$0")/j"
 "$__J"
 
 cd () {
-    "$__J --add $1 &" &>/dev/null
-    builtin cd "$1"
+	"$__J --add $1 &" &>/dev/null
+	builtin cd "$1"
 }
 ${J_CUSTOM_CMD:-j} () {
-    cd "$("$__J" "$1")"
+	cd "$("$__J" "$1")"
 }
