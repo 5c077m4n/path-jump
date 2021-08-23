98f0c0c9d654f3580bc029312ada1bc5c289381b
Roee Shapira <ro33.sha@gmail.com>
2021-08-24 00:54:40 +0300

    Added build env var

--- build.rs
+++ build.rs
@@ -1,12 +1,13 @@
-use std::{env, fs, io, path::Path};
+use std::{env, ffi::OsString, fs, io, path::Path};
 
 fn main() -> io::Result<()> {
 	let project_root = env::var_os("CARGO_MANIFEST_DIR").unwrap();
 	let target_profile = env::var_os("PROFILE").unwrap();
+	let target_dir = env::var_os("CARGO_TARGET_DIR").unwrap_or_else(|| OsString::from("target"));
 
 	let init_script = Path::new(&project_root).join("resources").join("j.sh");
 	let dest_path = Path::new(&project_root)
-		.join("target")
+		.join(&target_dir)
 		.join(&target_profile)
 		.join("j.sh");
 
