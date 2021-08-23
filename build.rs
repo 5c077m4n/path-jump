12e0ebca939032dd3d745286c20219210aba068e
Roee Shapira <ro33.sha@gmail.com>
2021-08-24 00:59:40 +0300

    Fixed path.

--- build.rs
+++ build.rs
@@ -4,6 +4,7 @@ fn main() -> io::Result<()> {
 	let project_root = env::var_os("CARGO_MANIFEST_DIR").unwrap();
 	let target_profile = env::var_os("PROFILE").unwrap();
 	let target_dir = env::var_os("CARGO_TARGET_DIR").unwrap_or_else(|| OsString::from("target"));
+	let target_triple = env::var_os("TARGET").unwrap();
 
 	let init_script = Path::new(&project_root).join("resources").join("j.sh");
 	let dest_path = Path::new(&project_root)
